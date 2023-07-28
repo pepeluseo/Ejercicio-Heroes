8c4aa16bb398c4d8bbef43c0f22d74ec43133c34
jose luis <pepeluseo@gmail.com>
2023-07-28 16:21:22 +0200

    app.module.ts: EstructuraModule, SharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+
+import localeES from '@angular/common/locales/es';
+import { registerLocaleData} from "@angular/common";
+registerLocaleData(localeES);
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SharedModule} from "./shared/shared.module";
+import {EstructuraModule} from "./estructura/estructura.module";
 
 @NgModule({
   declarations: [
@@ -10,9 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    EstructuraModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es'}
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
